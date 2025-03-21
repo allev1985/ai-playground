6233307fd0a68da59dc088cf4ce5557effc7ff00
allev1985 <alex@doshii.io>
2025-03-21 21:41:55 +1100

    removing all streamlist

--- streamlit/forms/personal_form.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def page():
-    st.title("Personal Details")
-    name = st.text_input("Name")
-    age = st.number_input("Age", min_value=0, max_value=100)
-    return {"name": name, "age": age}
